ee82921f67fc6a7403082a8a7e6aa290d6b8e2ca
alex <alexandruturcanu915@gmail.com>
2022-11-24 17:07:36 +0200

    Cleaned up file.

--- Assets/Project/Runtime/Entity/Animations/EntityAnimation.cs
+++ Assets/Project/Runtime/Entity/Animations/EntityAnimation.cs
@@ -1,47 +1,38 @@
 using System;
 using Project.Runtime.Utils;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Project.Runtime.Entity.Animations
 {
     [Serializable]
     public class EntityAnimation
     {
-        [SerializeField] private bool useDirection;
-        [SerializeField] private AnimationClip clip;
-
-        [SerializeField] private AnimationClip north;
-        [SerializeField] private AnimationClip south;
-        [SerializeField] private AnimationClip east;
-        [SerializeField] private AnimationClip west;
-
-        public bool UseDirection => useDirection;
-        public AnimationClip Clip => clip;
-        public AnimationClip North => north;
-        public AnimationClip South => south;
-        public AnimationClip East => east;
-        public AnimationClip West => west;
+        [field: SerializeField] public bool UseDirection { get; set; }
+        [field: SerializeField] public AnimationClip Clip { get; set; }
+        [field: SerializeField] public AnimationClip North { get; set; }
+        [field: SerializeField] public AnimationClip South { get; set; }
+        [field: SerializeField] public AnimationClip East { get; set; }
+        [field: SerializeField] public AnimationClip West { get; set; }
 
         public AnimationClip GetDirection(Direction currentDirection)
         {
-            return useDirection
+            const string nameOf = nameof(currentDirection);
+
+            return UseDirection
                 ? currentDirection switch
                 {
-                    Direction.S => south,
-                    Direction.E => east,
-                    Direction.N => north,
-                    Direction.W => west,
-                    Direction.SW => throw new ArgumentOutOfRangeException(nameof(currentDirection), currentDirection,
-                        null),
-                    Direction.SE => throw new ArgumentOutOfRangeException(nameof(currentDirection), currentDirection,
-                        null),
-                    Direction.NW => throw new ArgumentOutOfRangeException(nameof(currentDirection), currentDirection,
-                        null),
-                    Direction.NE => throw new ArgumentOutOfRangeException(nameof(currentDirection), currentDirection,
-                        null),
-                    _ => throw new ArgumentOutOfRangeException(nameof(currentDirection), currentDirection, null)
+                    Direction.S => South,
+                    Direction.E => East,
+                    Direction.N => North,
+                    Direction.W => West,
+                    Direction.SW => throw new ArgumentOutOfRangeException(nameOf, currentDirection, null),
+                    Direction.SE => throw new ArgumentOutOfRangeException(nameOf, currentDirection, null),
+                    Direction.NW => throw new ArgumentOutOfRangeException(nameOf, currentDirection, null),
+                    Direction.NE => throw new ArgumentOutOfRangeException(nameOf, currentDirection, null),
+                    _ => throw new ArgumentOutOfRangeException(nameOf, currentDirection, null)
                 }
-                : clip;
+                : Clip;
         }
     }
 }
